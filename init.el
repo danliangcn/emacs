580e6ef22f148bf789dec5d0e196dda2e51e465d
梁丹 <liangdan@liangdandeiMac.local>
2019-06-14 23:59:51 +0800

    place my config.org and init.el in github.

---
+++ init.el
@@ -0,0 +1,47 @@
+;;;; Dan's .emacs file
+; Dan Liang
+; 4 June 2019
+
+
+(require 'package)
+(setq package-enable-at-setup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;;;;;;
+;;;;;;
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+;;;;;;
+;;;;;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-enabled-themes (quote (spacemacs-dark)))
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(global-visual-line-mode t)
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages
+   (quote
+    (magit org-noter yasnippet-snippets yasnippet pretty-mode expand-region mark-multiple popup-kill-ring diminish spaceline dashboard switch-window avy smex ido-vertical-mode org-bullets beacon which-key use-package spacemacs-theme google-this org-ref pdf-tools org-pdfview auto-complete ess-smart-underscore ess auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "nil" :family "Menlo")))))
+
